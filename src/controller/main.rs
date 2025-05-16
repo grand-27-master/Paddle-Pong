2fd936631992804c9d1c604be7d7728310b773bd
Vashishth Gajjar <gajjarv2001@gmail.com>
2025-05-15 19:45:37 -0500

    author name updated

--- src/controller/main.rs
+++ src/controller/main.rs
@@ -5,7 +5,7 @@ use pong_game::model::constants::{WINDOW_WIDTH, WINDOW_HEIGHT};
 use pong_game::view::game::GameState;
 
 fn main() -> ggez::GameResult {
-    let (mut ctx, event_loop) = ContextBuilder::new("pong_game", "Your Name")
+    let (mut ctx, event_loop) = ContextBuilder::new("pong_game", "Vashishth Gajjar")
         .window_setup(WindowSetup::default().title("Paddle Pong"))
         .window_mode(WindowMode::default().dimensions(WINDOW_WIDTH, WINDOW_HEIGHT))
         .build()?;
